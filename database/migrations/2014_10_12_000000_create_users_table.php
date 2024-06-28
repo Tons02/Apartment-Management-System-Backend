643becaf6a25d8a89711164be541bdf06e5b4307
Antonio Montilla <system.montilla@gmail.com>
2024-06-28 09:24:42 +0800

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('contact_details')->nullable();
+            $table->enum("gender", ["male", "female"]);
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->unsignedInteger("role_id")->index();
+            $table->boolean('is_active')->default(true);
+
+            $table->foreign("role_id")
+            ->references("id")
+            ->on("roles")
+            ->onDelete('cascade');
+
+        
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
